12af463cdcd31ae5bc3d5cfe9a27ad657d689e63
laviksha <lavikshasoni@gmail.com>
2019-06-21 21:31:16 +0530

    Creates User Repository

---
+++ src/main/java/com/gfg/laviksha/SpringBootBackendService/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gfg.laviksha.SpringBootBackendService.Repository;
+
+import com.gfg.laviksha.SpringBootBackendService.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
